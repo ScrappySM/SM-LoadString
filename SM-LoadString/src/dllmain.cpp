021afea7d3033d0e87f76cb987ddcfec9c98b11c
Ben McAvoy <ben.mcavoy@tutanota.com>
2025-02-17 10:39:49 +0000

    refactor: code cleanup and reliable handling

--- SM-LoadString/src/dllmain.cpp
+++ SM-LoadString/src/dllmain.cpp
@@ -1,48 +1,32 @@
 #include <carbon/lua/lua.hpp>
 
 static int l_loadstring(lua_State* L) {
-    int n = lua_gettop(L);
+	int n = lua_gettop(L);
 
 	// Validate the number of arguments
-    if (n < 2) {
-		lua_pushliteral(L, "loadstring expected 2 arguments (string [code], table [environment]) got 0");
-        lua_error(L);
-    }
-    else if (n > 2) {
-		lua_pushfstring(L, "loadstring expected at most 2 arguments (string [code], table [environment]), got %d", n);
-		lua_error(L);
-    }
+	if (n != 2)
+		return luaL_error(L, "loadstring expected 2 arguments (string [code], table [environment]) got %d", n);
 
-	// Validate the environment, numbers can be passed to escape the environment
-    if (n > 1 && !lua_istable(L, 2)) {
-		lua_pushliteral(L, "second argument must be a table (environment)");
-        lua_error(L);
-    }
+	// Validate the environment, numbers can be passed to escape the environment so we make sure it's a table
+	luaL_checktype(L, 2, LUA_TTABLE);
 
-	const char* code = luaL_checkstring(L, 1); // Get the code from the first argument
+	size_t codeLen = 0;
+	const char* code = luaL_checklstring(L, 1, &codeLen); // Get the code from the first argument
 
-	// Check the code isn't bytecode, as this is a way to escape the environment
-	// Even if this is a user error, the user is most likely trying to escape the environment
-	// So they don't need the error message in the UI, they shouldn't be trying to be malicious
-	// if they want nice things.
-	if (strstr(code, LUA_SIGNATURE) != NULL) {
-		lua_pushliteral(L, "code cannot be bytecode");
-		lua_error(L);
+	if (codeLen >= 4 && strncmp(code, LUA_SIGNATURE, 4) == 0)
+		return luaL_error(L, "code cannot be bytecode");
+
+	// Attempt to load the code
+	if (luaL_loadstring(L, code) != LUA_OK) {
+		lua_pushnil(L); // No function loaded, return nil
+		lua_pushstring(L, lua_tostring(L, -2)); // Error
+		return 2; // nil, msg
 	}
 
-    if (luaL_loadstring(L, code) == LUA_OK) {
-		if (n > 1) {
-			lua_pushvalue(L, 2); // Bring the env table to the top
-			lua_setfenv(L, -2);
-		}
+	lua_pushvalue(L, 2); // Bring the env table to the top
+	lua_setfenv(L, -2); // Set the environment of the function (only valid for Lua 5.1, SM currently uses 5.1 so this is fine)
 
-        return 1;
-    }
-    else {
-		lua_pushnil(L); // No function loaded, return nil
-        lua_pushstring(L, lua_tostring(L, -2)); // Error
-        return 2;
-    }
+	return 1; // func, nil
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved) {
@@ -52,6 +36,12 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved) {
 		// Add the loadstring function to the sandboxed environment
 		LuaExecutor::GetInstance()->OnInitialize([](lua_State* L) {
 			lua_getglobal(L, "unsafe_env");
+
+			if (!lua_istable(L, -1)) {
+				lua_pop(L, 1);
+				return luaL_error(L, "unsafe_env not found");
+			}
+
 			lua_pushcclosure(L, l_loadstring, 0);
 			lua_setfield(L, -2, "loadstring");
 			lua_pop(L, 1);
